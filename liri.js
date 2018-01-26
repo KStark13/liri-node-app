4cc844c1f8ef698ab124737e69a453d7d9e1c8a7
Kate <kate.stark13@sbcglobal.net>
2018-01-26 11:11:18 -0600

    added switch statements

--- liri.js
+++ liri.js
@@ -4,11 +4,58 @@ var keys = require('./keys');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
-//liri commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'
+var fs = require('fs');
+
+//Arguments Array
+var nodeArgV = process.argv;
+var command = process.argv[2];
+
+//variable for the movie or song search
+var x = "";
+
+//attaches multiple word arguents
+for (var i = 3; i < nodeArgV.length; i++) {
+	if(i>3 && i<nodeArgV.length) {
+		x = x + "+" + nodeArgV[i];
+	} else{
+		x = x + nodeArgV[i];
+	}
+}
+//Switch Commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'
+switch(command){
+	case "my-tweets":
+		showTweets();
+	break;
+
+	case "spotify-this-song":
+		if(x) {
+			spotifySong(x);
+		} else {
+			spotifySong("The Sign");
+		}
+	break;
+
+	case "movie-this":
+		if(x) {
+			omdbData(x);
+		} else {
+			omdbData("Mr. Nobody")
+		}
+	break;
+
+	case "do-what-it-says":
+		doThing();
+	break;
+
+	default:
+	console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}")
+
+}
+
 
 //my-tweets: get my last 20 tweets (get request by user)
-
-var client = new Twitter(keys.twitter);
+function showTweets() {
+	var client = new Twitter(keys.twitter);
 var params= {
 	screen_name: 'Stark13Kate' 
 };
@@ -19,36 +66,37 @@ function gotData (error, tweets, response) {
     	var created = tweets[i].created_at;
     	var message = tweets[i].text;
     	console.log("-------------------")
-    	console.log(created.substring(0, 16));
-    	console.log(message);
+    	console.log("Date: " + created.substring(0, 16));
+    	console.log("Tweet: " + message);
     };	 
 };
+}
 
 
-//////////////////////SPOTIFY///////////////////////////////////////
 
 //spotify-this-song: get [artist, song name, preview link, album song is from]; 
 //if no song is chosen, it will default to "The Sign" by Ace of Base
-
-var spotify = new Spotify(keys.spotify);
-spotify.search({ type: 'track', query: "Ironic", limit: 1 }, function(err, data) {
-for (var i = 0; i < data.tracks.items.length; i++) {
+function spotifySong(song) {
+	var spotify = new Spotify(keys.spotify);
+	spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+	for (var i = 0; i < data.tracks.items.length; i++) {
 	var songData = data.tracks.items[i];
-	console.log(songData.artists[0].name);
-	console.log(songData.name);
-	console.log(songData.preview_url);
-	console.log(songData.album.name)
-}
- 
- /*	console.log(data.tracks);*/
+	console.log("Artist: " + songData.artists[0].name);
+	console.log("Song Title: " + songData.name);
+	console.log("Preview Link: " + songData.preview_url);
+	console.log("Album: " + songData.album.name)
+	}
 });
+}
+
 
 
 
 //movie-this: will output [title, year relased, IMDb rating, Rotten Tomatoes rating, 
 //contry it was produced, language, plot, actors];
 //If user doesnt type in movie, will default to "Mr. Nobody"
-var omdbURL = 'http://www.omdbapi.com/?t=star+wars&plot=short&tomatoes=true&apikey=trilogy';
+function omdbData(movie){
+		var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true&apikey=trilogy';
 request(omdbURL, function(error, response, body){
 	var body = JSON.parse(body);
 
@@ -61,6 +109,13 @@ console.log("Language: " + body.Language);
 console.log("Plot: " + body.Plot);
 console.log("Actors: " + body.Actors);
 });
-
+}
 //do-what-it-says will take text from random.txt and use it to call one of LIRIs commands.
 //Will run spotify-this-song for "I Want it That Way"
+function doThing(){
+	fs.readFile('random.txt', "utf8", function(error, data){
+	var txt = data.split(",");
+	
+	spotifySong(txt[1]);
+})
+}
